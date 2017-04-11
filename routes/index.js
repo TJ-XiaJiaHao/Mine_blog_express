df6c7c27aa2f578cd91fdd51ec8c70ebf78ce180
XJH <598056288@qq.com>
2017-04-11 09:19:23 +0800

    0411

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/home', { title: '个人主页' });
+  res.render('home/index', { title: '个人主页' });
+});
+router.get('/home/info', function(req, res, next) {
+    res.render('home/home', { title: '详细简历' });
 });
 
 module.exports = router;
